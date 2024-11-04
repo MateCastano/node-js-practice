7154b10c64800c3a6727276a2fc26281856f31cb
MateCastano <mateonc@gmail.com>
2024-11-04 12:06:31 -0300

    HTTP and Mongo configured

---
+++ src/config/express.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+const expressApp = express();
+
+//TO DO middelwares, route, etc...
+
+export default expressApp;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import { printHello } from '#Lib/hello.js';
+import connectDB from '#Config/db.js';
+import '#Config/env.js';
+import httpServer from "#Config/https.js";
+import { connect } from 'mongoose';
 
-printHello();
+const bootstrap = async () => {
+    await connectDB(process.env.MONGODB_URL);
+    
+    httpServer.listen(process.env.PORT, ()=>{console.log('Servidor en puerto 3000')});
+}
+
+bootstrap();
