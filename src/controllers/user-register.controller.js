64fa6cb26b07540363a566e1b23c7d69d306bf67
Mateo <mateo@gmail.com>
2025-03-13 12:44:34 -0300

    feat. sendMail() (need fixes)

--- src/controllers/user-register.controller.js
+++ src/controllers/user-register.controller.js
@@ -1,23 +1,49 @@
 import userModel from "#Schemas/user.schema.js";
 import { hash } from "bcrypt";
+import nodemailer from "nodemailer";
 
 const userRegisterController = async (req, res) =>{
-    const {_id, name, surname, email, password} = req.body;
+   const {_id, name, surname, email, password} = req.body;
 
-     const existingUserById = await userModel.findById(_id).exec();
-     if(existingUserById) return res.status(409).send({errors: ['Ya existe un usuuario con ese mail registrado.']});
+   const existingUserById = await userModel.findById(_id).exec();
+   if(existingUserById) return res.status(409).send({errors: ['Ya existe un usuuario con ese mail registrado.']});
 
-     const existingUserByEmail = await userModel.findOne({email}).exec();
-     if(existingUserByEmail) return res.status(409).send({errors: ['Ya existe un usuuario con ese mail registrado.']});
+   const existingUserByEmail = await userModel.findOne({email}).exec();
+   if(existingUserByEmail) return res.status(409).send({errors: ['Ya existe un usuuario con ese mail registrado.']});
 
-     const hashedPassword = await hash(password, 12);
-     const user = new userModel({
-        _id, name, surname, email, password: hashedPassword
-     })
+   const hashedPassword = await hash(password, 12);
+   const user = new userModel({
+      _id, name, surname, email, password: hashedPassword
+   })
 
-     await user.save();
+   const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth:{
+         user: process.env.EMAIL_USER,
+         pass: process.env.EMAIL_PASSWORD
+      }
+   })
+   const sendEmail = async () => {
+      try {
+         const info = await transporter.sendMail({
+              from: `"Tu Nombre" <${process.env.EMAIL_USER}>`,
+              to: email,
+              subject: "Asunto del correo",
+              text: "Llego el email.",
+              html: "<b>Este es el contenido en HTML</b>",
+          });
+  
+          console.log("Email enviado: " + info.messageId);
+      } catch (error) {
+          console.error("Error enviando el email:", error);
+      }
+   };
+  
+   sendEmail();
+     
+   await user.save();
 
-     return res.status(201).send('Usuario registrado con exito.')
+   return res.status(201).send('Usuario registrado con exito.')
 }
 
 export default userRegisterController;
